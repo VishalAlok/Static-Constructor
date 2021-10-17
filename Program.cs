c9dedab6ac525235bc437b6cb640c75cc7a3ec63
vishal.Alok <you@example.com>
2021-10-17 21:26:00 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace staticConstructorExample
+{
+    class geeks
+    {
+        static geeks()
+        {
+            Console.WriteLine("Static Constructor");
+        }
+
+        // Instance constructor.
+        public geeks(int i)
+        {
+            Console.WriteLine("Instance Constructor " + i);
+        }
+
+        // Instance method.
+        public string geeks_detail(string name, int id)
+        {
+            return "Name:" + name + " id:" + id;
+        }
+        public static void Main()
+        {
+
+            //Both Static and instance constructors are invoked
+            geeks obj = new geeks(1);
+
+            //Only instance constructor will be invoked
+            geeks obj1 = new geeks(2);
+        }
+    }
+}
